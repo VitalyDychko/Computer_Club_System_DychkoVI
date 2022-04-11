492b4b706df31a182e5780efdce1f85c3ce6e0af
Vitaly Alexandreetsky <Dartveider233@gmail.com>
2022-04-12 03:32:13 +0500

    Новая волна изменений №2

--- Computer_Club_System_DychkoVI/UserList.Designer.cs
+++ Computer_Club_System_DychkoVI/UserList.Designer.cs
@@ -261,6 +261,7 @@ namespace Dyczko_ComputerClub_System
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.Size = new System.Drawing.Size(780, 415);
             this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
             // 
             // toolStrip1
             // 
